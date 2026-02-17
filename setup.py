0ae465c74a0812049afb45d11c54fc4d3b2ff7df
Meesum Qazalbash <meesumqazalbash@gmail.com>
2026-02-17 16:41:56 +0500

    Change test dependencies to .tar.gz URLs
    Updated test dependencies to use .tar.gz URLs instead of tarball.

--- setup.py
+++ setup.py
@@ -37,9 +37,9 @@ setup(
         'test': [
             'flake8',
             'pytest>=5.0',
-            'pyro-ppl@https://api.github.com/repos/pyro-ppl/pyro/tarball/dev',
-            'numpyro@https://api.github.com/repos/pyro-ppl/numpyro/tarball/master',
-            'funsor@https://api.github.com/repos/pyro-ppl/funsor/tarball/master',
+            'pyro-ppl@https://api.github.com/repos/pyro-ppl/pyro/tarball/dev.tar.gz',
+            'numpyro@https://api.github.com/repos/pyro-ppl/numpyro/tarball/master.tar.gz',
+            'funsor@https://api.github.com/repos/pyro-ppl/funsor/tarball/master.tar.gz',
         ],
         'dev': [
             'sphinx>=2.0',
